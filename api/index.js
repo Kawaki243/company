d112aab92da4ef25c2aa24611c132d4949e0574b
Pham Tran Minh Duc <ducprodapchai243@gmail.com>
2024-02-11 17:16:13 +0100

    First Commit

--- api/index.js
+++ api/index.js
@@ -3,9 +3,6 @@ import mysql from "mysql2";
 import cors from "cors";
 import bcrypt from "bcrypt"
 import jwt from "jsonwebtoken";
-import dotenv from "dotenv";
- 
-dotenv.config();
 const app = express();
 const PORT = 8080;
  
@@ -13,10 +10,11 @@ app.use(express.json());
 app.use(cors());
  
 const db = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE
+    host: "localhost",
+    user: "root",
+    port: 3306,
+    password: "Dangcapmaimai123",
+    database: "tranpham"
 });
  
 db.connect(function(err) {
